4a2b59f1f38a03a0815e9dbc5640e7664b520b55
Davron <davron_sattorov_99@mail.ru>
2021-12-05 10:19:06 +0200

    Firebase fixed

--- utils/firebase.js
+++ utils/firebase.js
@@ -5,12 +5,18 @@ import { getFirestore } from "@firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA3NmrroEWw8vCA-cEFDF1xopFIlFeHKxk",
-  authDomain: "medit-c2842.firebaseapp.com",
-  projectId: "medit-c2842",
-  storageBucket: "medit-c2842.appspot.com",
-  messagingSenderId: "496752970830",
-  appId: "1:496752970830:web:e13d1c47700e46869fc71f",
+  // apiKey: "AIzaSyA3NmrroEWw8vCA-cEFDF1xopFIlFeHKxk",
+  // authDomain: "medit-c2842.firebaseapp.com",
+  // projectId: "medit-c2842",
+  // storageBucket: "medit-c2842.appspot.com",
+  // messagingSenderId: "496752970830",
+  // appId: "1:496752970830:web:e13d1c47700e46869fc71f",
+  apiKey: "AIzaSyCaBklnYJLlDwEDZepA7rznNwWFWOaKToE",
+  authDomain: "medit-29b70.firebaseapp.com",
+  projectId: "medit-29b70",
+  storageBucket: "medit-29b70.appspot.com",
+  messagingSenderId: "950604660520",
+  appId: "1:950604660520:web:662f2f6911bb5eb3b25a1d",
 };
 
 export const firebase = initializeApp(firebaseConfig);
